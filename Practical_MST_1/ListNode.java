7bdf55eec4ad94fbdfd379b22e762aa444ae399b
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-03-10 10:14:11 +0530

    mst 1 - solution code added.

---
+++ Practical_MST_1/ListNode.java
@@ -0,0 +1,50 @@
+/**
+ * Leetcode 86
+ * 
+ */
+
+//   Definition for singly-linked list.
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode ld = new ListNode(0);
+        ListNode gd = new ListNode(0);
+
+        ListNode l = ld;
+        ListNode g = gd;
+
+        while (head != null) {
+            if (head.val < x) {
+                l.next = head;
+                l = l.next;
+            } else {
+                g.next = head;
+                g = g.next;
+            }
+            head = head.next;
+        }
+
+        g.next = null;
+        l.next = gd.next;
+
+        return ld.next;
+    }
+}
